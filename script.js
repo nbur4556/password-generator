9f6f32f5437546a58e30ae46fec95a5c87b6b714
nbur4556 <nbur4556@gmail.com>
2020-07-31 09:05:54 -0500

    created array of useable characters based on password requirements

--- script.js
+++ script.js
@@ -19,9 +19,52 @@ function setPasswordOptions() {
   passwordOptions.passwordLength = prompt("How long should the password be?")
 }
 
+// Returns an array of useable characters depending on options from passwordOptions
+function buildUseableCharactersArray() {
+  var useableCharacters = [];
+  var allLetters = [
+    "a", "b", "c", "d", "e", "f", "g", "h",
+    "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x",
+    "y", "z"
+  ];
+  var allSpecialCharacters = [
+    "!", "#", "$", "%", "&", "'", "(", ")",
+    "*", "+", ",", "-", ".", "/", ":", ";",
+    "<", "=", ">", "?", "@", "[", "]", "^",
+    "_", "`", "{", "|", "}", "~"
+  ]
+
+  if (passwordOptions.useLowercase === true) {
+    for (let i = 0; i < allLetters.length; i++) {
+      useableCharacters.push(allLetters[i]);
+    }
+  }
+  if (passwordOptions.useUppercase === true) {
+    for (let i = 0; i < allLetters.length; i++) {
+      useableCharacters.push(allLetters[i].toUpperCase());
+    }
+  }
+  if (passwordOptions.useNumbers === true) {
+    for (let i = 0; i < 10; i++) {
+      useableCharacters.push(0 + i);
+    }
+  }
+  if (passwordOptions.useSpecialCharacters === true) {
+    for (let i = 0; i < allSpecialCharacters.length; i++) {
+      useableCharacters.push(allSpecialCharacters[i]);
+    }
+  }
+
+  return useableCharacters;
+}
+
 // Runs when 'Generate Password' button is pressed
 function generatePassword() {
   setPasswordOptions();
+  var useableCharacters = buildUseableCharactersArray();
+
+  console.log(useableCharacters);
 }
 
 // Write password to the #password input
